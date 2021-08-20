d0d56afbdc85f75055643be7e282ae7395228799
Eric Leong <eric-213@hotmail.com>
2021-08-20 15:29:22 +0800

    detect Dimension and orientation

--- App.js
+++ App.js
@@ -1,13 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+
+// npm install @react-native-community/hooks first
+import { useDeviceOrientation, useDimensions } from '@react-native-community/hooks';
+
 
 export default function App() {
+
+  // Object destructuring, landscape is the property inside the object
+  const { landscape } = useDeviceOrientation();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={{
+        backgroundColor: "dodgerblue",
+        width: '100%',
+
+        // Change the height as the orientation change
+        height: landscape ? "100%" : "30%",
+
+      }}>
+        <Text>Mosh is with us, thanks Mosh</Text>
+      </View>
+    </SafeAreaView>
   );
 }
 
