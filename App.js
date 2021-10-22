1b902140512b1f5d4e7f26bf11f26509d177dac5
Eric Leong <eric-213@hotmail.com>
2021-10-23 00:10:55 +0800

    access camera library

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
-import React, { useEffect } from "react";
-import { StyleSheet, Text, TextInput, View, Switch } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, Button, Image } from "react-native";
 
 import Screen from "./components/Screen";
 import * as ImagePicker from "expo-image-picker";
+import * as Permissions from "expo-permissions";
 
 export default function App() {
+  const [imageUri, setImageUri] = useState();
+
+  const selectImage = async () => {
+    try {
+      const result = await ImagePicker.launchImageLibraryAsync();
+      if (!result.cancelled) setImageUri(result.uri);
+    } catch (error) {
+      console.log("error reading an image", error);
+    }
+  };
+
   const requestPermission = async () => {
-    const result = await ImagePicker.requestCameraPermissionsAsync();
-    if (!result.granted) alert("You need to enable access permission");
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("not permitted");
   };
 
   useEffect(() => {
@@ -16,7 +28,8 @@ export default function App() {
 
   return (
     <Screen>
-      <Text>hi23</Text>
+      <Button title="Select Image" onPress={selectImage} />
+      <Image source={{ uri: imageUri }} style={{ width: 200, height: 200 }} />
     </Screen>
   );
 }
