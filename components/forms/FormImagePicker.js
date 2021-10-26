af54cb821fb07d0c9401eed12eb1857959246456
Eric Leong <eric-213@hotmail.com>
2021-10-26 17:00:25 +0800

    FormImagePicker finished

---
+++ components/forms/FormImagePicker.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+import ErrorMessage from "./ErrorMessage";
+import ImageInputList from "../ImageInputList";
+
+import { useFormikContext } from "formik";
+
+
+const FormImagePicker = ({ name }) => {
+  const { errors, touched, values, setFieldValue } = useFormikContext();
+  const imageUris = values[name];
+
+  const handleAdd = (uri) => {
+    setFieldValue(name, [...imageUris, uri]);
+  };
+  const handleRemove = (uri) => {
+    setFieldValue(imageUris.filter((imageUri) => imageUri != uri));
+  };
+
+
+  return (
+  <>
+  <ImageInputList
+   imageUris={imageUris}
+   onAddImage={handleAdd}
+   onRemoveImage={handleRemove}
+  />
+  <ErrorMessage error={errors[name]} visible={touched[name]} />
+  </>);
+};
+
+export default FormImagePicker;
