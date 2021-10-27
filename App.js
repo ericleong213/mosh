e088dc157d701d3bb74f652f68167d1641a59a37
Eric Leong <eric-213@hotmail.com>
2021-10-27 17:58:36 +0800

    Stack Navigator done

--- App.js
+++ App.js
@@ -1,13 +1,43 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Text, Button, Imag, FlatList } from "react-native";
-import ListingEditScreen from "./screens/ListingEditScreen";
+import { StyleSheet, Text} from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import Screen from "./components/Screen";
 
 
+const Tweets =() => {
+  <Screen>
+    <Text>Tweets</Text>
+  </Screen>
+};
+
+const TweetsDetails = () => {
+  <Screen>
+    <Text>Tweet Details</Text>
+  </Screen>
+}
+
+const Stack = createStackNavigator();
+const StackNavigator = () => {
+  <Stack.Navigator initialRouteName="Tweets">
+    <Stack.Screen 
+    name="Tweets"
+    component={Tweets}
+    />
+    <Stack.Screen 
+    name="TweetsDetails"
+    component={TweetsDetails}
+    />
+  </Stack.Navigator>
+}
 
 export default function App() {
   
 
   return (
-    <ListingEditScreen/>
+    <NavigationContainer>
+      <StackNavigator/>
+    </NavigationContainer>
   );
 }
--- hooks/useLocation.js
+++ hooks/useLocation.js
@@ -5,28 +5,30 @@ import * as Location from 'expo-location';
 
 export default useLocation = () => {
 
-    const [location, setLocation] = useState();
-
-    const getLocation = async () => {
-
-        try {
-            
-            const { granted } = await Location.requestForegroundPermissionsAsync();
-            if (!granted) return;
-            const {
-              coords: { latitude, longitude },
-            } = await Location.getLastKnownPositionAsync();
-            setLocation({ latitude, longitude });
-          
-        } catch (error) {
-            
-            console.log(error);
-        }
-
+  const [location, setLocation] = useState();
   
-    useEffect(() => {
-      getLocation();
-    }, []);
+  const getLocation = async () => {
+  
+      try {
+          
+          const { granted } = await Location.requestForegroundPermissionsAsync();
+          if (!granted) return;
+          const {
+            coords: { latitude, longitude },
+          } = await Location.getLastKnownPositionAsync();
+          setLocation({ latitude, longitude });
+        
+      } catch (error) {
+          
+          console.log(error);
+      }
+  
+  
+  useEffect(() => {
+    getLocation();
+  }, []);
+  
+  return location;
+  }
+}
 
-    return location;
-};
