01ee1a60859512689c1e3fcab3c89adde7eb3801
Eric Leong <eric-213@hotmail.com>
2021-10-28 00:12:15 +0800

    pass parameters to route finished

--- App.js
+++ App.js
@@ -1,43 +1,54 @@
-import React, { useEffect, useState } from "react";
-import { StyleSheet, Text} from "react-native";
+import React from "react";
+import { Text, Button } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
-import { NavigationContainer } from "@react-navigation/native";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./components/Screen";
 
+//need to add return () to wrap the component such as Tweets, TweetsDetails and StackNavigator. This is not necessary in Mosh course. This problem may be caused by different version.
 
-const Tweets =() => {
-  <Screen>
-    <Text>Tweets</Text>
-  </Screen>
+//navigation parameter only available in the Screen component but not its child component, need to use useNavigation() or pass the props one by one, I prefer the first method.
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button
+      title="click"
+      onPress={() => navigation.navigate("TweetsDetails", { id: 1 })}
+    />
+  );
 };
 
-const TweetsDetails = () => {
-  <Screen>
-    <Text>Tweet Details</Text>
-  </Screen>
-}
+const Tweets = ({ navigation }) => {
+  return (
+    <Screen>
+      <Text>Tweets</Text>
+      <Link />
+    </Screen>
+  );
+};
+
+const TweetsDetails = ({ route }) => {
+  return (
+    <Screen>
+      <Text>Tweet Details{route.params.id}</Text>
+    </Screen>
+  );
+};
 
 const Stack = createStackNavigator();
 const StackNavigator = () => {
-  <Stack.Navigator initialRouteName="Tweets">
-    <Stack.Screen 
-    name="Tweets"
-    component={Tweets}
-    />
-    <Stack.Screen 
-    name="TweetsDetails"
-    component={TweetsDetails}
-    />
-  </Stack.Navigator>
-}
+  return (
+    <Stack.Navigator initialRouteName="Tweets">
+      <Stack.Screen name="Tweets" component={Tweets} />
+      <Stack.Screen name="TweetsDetails" component={TweetsDetails} />
+    </Stack.Navigator>
+  );
+};
 
 export default function App() {
-  
-
   return (
     <NavigationContainer>
-      <StackNavigator/>
+      <StackNavigator />
     </NavigationContainer>
   );
 }
