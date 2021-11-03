dbf48b42adc519c2aeb7e0fe1a02ba1f92916f16
Eric Leong <eric-213@hotmail.com>
2021-11-03 23:26:57 +0800

    customize navigation theme

--- App.js
+++ App.js
@@ -1,13 +1,11 @@
 import React from "react";
-import { View, Text, Button } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import AuthNavigator from "./navigation/AuthNavigator";
+import navigationTheme from "./navigation/navigationTheme";
 
 export default function App() {
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
       <AuthNavigator />
     </NavigationContainer>
   );
