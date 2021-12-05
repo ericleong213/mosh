abe0953a58d78a2ac9e262f749e92f2e8cad1dfe
Eric Leong <eric-213@hotmail.com>
2021-12-05 23:57:08 +0800

    async storage with inspection
    introduction to async storage and inspect cache with react-native debugger

--- App.js
+++ App.js
@@ -4,11 +4,35 @@ import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import AuthNavigator from "./navigation/AuthNavigator";
 import navigationTheme from "./navigation/navigationTheme";
 import AppNavigator from "./navigation/AppNavigator";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
+
+  const storage = async () => {
+
+  
+  try {
+  await AsyncStorage.setItem('person', JSON.stringify({id: 1}) )
+  const value = await AsyncStorage.getItem('person')
+  const person = JSON.parse(value)
+  console.log(person)
+    
+  } catch (error) {
+    console.log(error)
+  }
+
+
+  }
+
+  storage()
+
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
-  );
+    <View style={{ flex:1, backgroundColor: "black"}}></View>
+  )
+    
+  // <NavigationContainer theme={navigationTheme}>
+  //   <AppNavigator />
+  // </NavigationContainer>
 }
