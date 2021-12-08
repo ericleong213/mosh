a2fa2311ecff5eb33ad7085a6111a4a1144b52ec
Eric Leong <eric-213@hotmail.com>
2021-12-09 00:45:00 +0800

    implement cache layer
    add cache in client.js, and redefine the function of apiClient.get.

--- App.js
+++ App.js
@@ -9,30 +9,9 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 
 export default function App() {
-
-  const storage = async () => {
-
-  
-  try {
-  await AsyncStorage.setItem('person', JSON.stringify({id: 1}) )
-  const value = await AsyncStorage.getItem('person')
-  const person = JSON.parse(value)
-  console.log(person)
-    
-  } catch (error) {
-    console.log(error)
-  }
-
-
-  }
-
-  storage()
-
   return (
-    <View style={{ flex:1, backgroundColor: "black"}}></View>
-  )
-    
-  // <NavigationContainer theme={navigationTheme}>
-  //   <AppNavigator />
-  // </NavigationContainer>
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
 }
--- api/client.js
+++ api/client.js
@@ -1,7 +1,24 @@
 import { create } from "apisauce";
+import cache from "../utility/cache";
 
 const apiClient = create({
   baseURL: "https://60cdf77891cc8e00178dc3a7.mockapi.io/api/v1",
 });
+
+const get = apiClient.get;
+
+apiClient.get = async (url, params, axiosConfig) => {
+  //this is how to change the implementation of get method
+  //Before the original function
+  const response = await get(url, params, axiosConfig);
+  //After the original function
+  if (response.ok) {
+    cache.get(url, response.data);
+    return response;
+  }
+
+  const data = await cache.get(url);
+  return data ? { ok: true, data } : response;
+};
 
 export default apiClient;
