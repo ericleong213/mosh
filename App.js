4bed439eec07838000e75e35197df3ccb4b74ddc
Eric Leong <eric-213@hotmail.com>
2021-12-13 00:44:28 +0800

    offline notice

--- App.js
+++ App.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { View, Text, Image } from "react-native";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import AuthNavigator from "./navigation/AuthNavigator";
+
 import navigationTheme from "./navigation/navigationTheme";
 import AppNavigator from "./navigation/AppNavigator";
-import AsyncStorage from "@react-native-async-storage/async-storage";
-
-import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+import OfflineNotice from "./components/OfflineNotice";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
---
+++ components/OfflineNotice.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+import colors from "../config/colors";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
+
+function OfflineNotice(props) {
+  const netInfo = useNetInfo();
+  console.log(netInfo);
+
+  if (netInfo.type !== "unknown" && netInfo.isInternetReachable === false)
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>No Internet connection</Text>
+      </View>
+    );
+
+  return null;
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors.primary,
+    height: 50,
+    width: "100%",
+    position: "absolute",
+    zIndex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "white",
+  },
+});
+
+export default OfflineNotice;
